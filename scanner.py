93d2d53e9df46c9d68b8868324bcaafcf4f65726
Keith Jordan <keithyjordan11@gmail.com>
2024-08-27 08:22:11 -0500

    updated

--- scanner.py
+++ scanner.py
@@ -5,38 +5,25 @@ import nmap
 scanner = nmap.PortScanner()
 
 print("Welcome, this is a simple nmap automation tool")
-print("<-------------------------------------------------------->")
+print("<----------------------------------------------------->")
 
 ip_addr = input("Please enter the IP address you want to scan: ")
-print("The IP you entered is: " , ip_addr)
+print("The IP you entered is: ", ip_addr)
 type(ip_addr)
 
-resp = input(""" \nPlease enter the type of scan you want to run
+resp = input("""\nPlease enter the type of scan you want to run
                 1)SYN ACK Scan
                 2)UDP Scan
                 3)Comprehensive Scan \n""")
 print("You have selected option: ", resp)
-
-if resp == '1':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sS')
+resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
+if resp not in resp_dict.keys():
+    print("enter a valid option")
+else:
+    print("nmap version: "sccaner.nmap_version())
+    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
     print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-elif resp == '2':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sU')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
-elif resp == '3':
-    print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-elif resp >= '4':
-    print("Please enter valid option")
+    if scanner.scaninfo()=='up':
+        print("Scanner Status: ",scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
